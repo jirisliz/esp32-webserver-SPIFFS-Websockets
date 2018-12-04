8c8b5aff7384c0d5cb0c019212bf1ce7aaac6c3d
Jiří Sliž <sliz@biovendor.com>
2018-12-04 10:56:18 +0100

    example added. Prepared for dimming led using slider and websockets.

---
+++ examples/ESP32WebserverSPIFFSWebsockets_testLED/ESP32WebserverSPIFFSWebsockets_testLED.ino
@@ -0,0 +1,33 @@
+/*
+   WebSocketServer.ino
+
+    Created on: 22.05.2015
+
+*/
+
+#include <Arduino.h>
+#include "heartBeat.h"
+#include "esp32WebserverSPIFFSWebsockets.h"
+
+#define USE_SERIAL Serial
+
+const char* ssid     = "BioVendorIns";
+const char* password = "H3sloB101NS#";
+
+HeartBeat heart(LED_BUILTIN);
+Esp32WebserverSPIFFS server(ssid, password);
+
+void setup() {
+  delay(2000); 
+  
+  USE_SERIAL.begin(115200);
+  USE_SERIAL.setDebugOutput(true);
+
+  server.init();
+  
+}
+
+void loop() {
+  heart.run();
+  //server.loop();
+}
